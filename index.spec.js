4a1cd49e68ad6aeb8e63dc10cd49be197c1f448e
Julien Fontanet <julien.fontanet@isonoe.net>
2015-05-11 11:54:39 +0200

    Unit tests.

--- index.spec.js
+++ index.spec.js
@@ -4,6 +4,130 @@
 
 // ===================================================================
 
+var Bluebird = require('bluebird')
+var eventToPromise = require('event-to-promise')
+var expect = require('must')
+var http = require('http')
+
+var httpProxyRequest = require('./')
+
+// ===================================================================
+
+Bluebird.longStackTraces()
+
+function drainStream (stream) {
+  var chunks = []
+  var length = 0
+
+  var resolve, reject
+  var promise = new Bluebird(function (resolve_, reject_) {
+    resolve = resolve_
+    reject = reject_
+  })
+
+  function onData (chunk) {
+    chunks.push(chunk)
+    length += chunk.length
+  }
+  stream.on('data', onData)
+
+  function onEnd () {
+    resolve(String(Buffer.concat(chunks, length)))
+
+    clean()
+  }
+  stream.on('end', onEnd)
+
+  function onError (error) {
+    reject(error)
+
+    clean()
+  }
+  stream.on('error', onError)
+
+  function clean () {
+    length = chunks.length = 0
+
+    stream.removeListener('data', onData)
+    stream.removeListener('end', onEnd)
+    stream.removeListener('error', onError)
+  }
+
+  return promise
+}
+
+// ===================================================================
+
 describe('httpProxyRequest', function () {
-  // TODO
+  var proxy
+  var proxyPort
+
+  var server
+  var serverPort
+
+  before(function () {
+    proxy = http.createServer(function (req, rep) {
+      httpProxyRequest({
+        hostname: 'localhost',
+        port: serverPort
+      }, req, rep)
+    }).listen(0, 'localhost')
+
+    server = http.createServer().listen(0, 'localhost')
+
+    return Bluebird.all([
+      eventToPromise(proxy, 'listening'),
+      eventToPromise(server, 'listening')
+    ]).then(function () {
+      proxyPort = proxy.address().port
+      serverPort = server.address().port
+    })
+  })
+
+  after(function () {
+    proxy.close()
+    server.close()
+  })
+
+  function makeRequest (opts) {
+    if (!opts) {
+      opts = {}
+    }
+
+    var req = http.request({
+      hostname: 'localhost',
+      method: opts.method,
+      port: proxyPort
+    })
+
+    if (opts.stream) {
+      opts.stream.pipe(req)
+    } else {
+      req.end(opts.body)
+    }
+
+    return eventToPromise(req, 'response')
+  }
+
+  // =================================================================
+
+  it('forwards requests', function () {
+    return Bluebird.all([
+      makeRequest({
+        body: 'ping',
+        method: 'POST'
+      }).then(function (res) {
+        return drainStream(res).then(function (body) {
+          expect(body).to.equal('pong')
+        })
+      }),
+      eventToPromise(server, 'request').spread(function (req, res) {
+        res.end('pong')
+
+        return drainStream(req).then(function (body) {
+          expect(body).to.equal('ping')
+        })
+      })
+    ])
+  })
 })
